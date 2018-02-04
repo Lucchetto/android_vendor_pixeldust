nitrogen-project/android_vendor_nitrogen

add support for building custom themes

products/themes.mk
@@ -1,0 +1,34 @@
+# Theme vendor extension
+-include vendor/nitrogen/themes/Android.mk
+
+# Deep Purple
+PRODUCT_PACKAGES += \
+    DeepPurpleTheme
+	
+# Indigo
+PRODUCT_PACKAGES += \
+    IndigoTheme
+	
+# Pink
+PRODUCT_PACKAGES += \
+    PinkTheme
+	
+# Purple
+PRODUCT_PACKAGES += \
+    PurpleTheme
+	
+# Red
+PRODUCT_PACKAGES += \
+    RedTheme
+	
+# Sky Blue
+PRODUCT_PACKAGES += \
+    SkyBlueTheme
+	
+# Yellow
+PRODUCT_PACKAGES += \
+    YellowTheme
+	
+# Teal
+PRODUCT_PACKAGES += \
+    TealTheme

Lucchetto/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi77@gmx.com>

configs/themes.mk
@@ -1,0 +1,34 @@
+# Theme vendor extension
+-include vendor/pixeldust/themes/Android.mk
+
+# Deep Purple
+PRODUCT_PACKAGES += \
+    DeepPurpleTheme
+	
+# Indigo
+PRODUCT_PACKAGES += \
+    IndigoTheme
+	
+# Pink
+PRODUCT_PACKAGES += \
+    PinkTheme
+	
+# Purple
+PRODUCT_PACKAGES += \
+    PurpleTheme
+	
+# Red
+PRODUCT_PACKAGES += \
+    RedTheme
+	
+# Sky Blue
+PRODUCT_PACKAGES += \
+    SkyBlueTheme
+	
+# Yellow
+PRODUCT_PACKAGES += \
+    YellowTheme
+	
+# Teal
+PRODUCT_PACKAGES += \
+    TealTheme